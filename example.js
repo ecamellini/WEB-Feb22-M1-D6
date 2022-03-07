babdd7b428844fb5f1caf6f8d1585b8907e7b191
Eric Camellini <eric.camellini@gmail.com>
2022-03-07 13:30:05 +0100

    First DOM manipulation examples

---
+++ example.js
@@ -0,0 +1,26 @@
+
+
+
+let DOM = {
+    head: {},
+    body: {
+        h1: "Title of your page",
+        div: {
+            span: "Hello I am a span",
+            a: {
+                content: "Link to page",
+                href: "https://example.com"
+            }
+        }
+    }
+}
+
+DOM.body
+
+
+student = {
+    name: "John"
+}
+
+student.name = "Ahmed"
+
